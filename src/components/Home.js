2eb55b2c85d18e9158a7eeeed2401b4f3531dbaa
BenWix <44454993+BenWix@users.noreply.github.com>
2020-09-15 10:45:09 -0400

    updates styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,13 @@ const Home = () => {
         <div className='text'>
             <h1>Hey, I'm Ben</h1>
             {/* Switch to I statements */}
-            <h3>As a software engineer and full-stack web developer with a 
+            <p>I am a software engineer and full-stack web developer with a 
                 background in mechanical engineering and design, I have an 
                 eagerness to tackle difficult coding challenges. I am  great
                 problem solver and critical thinker with excellent project 
                 management abilities. I have experience building full-stack 
                 web applications utilizing Ruby on Rails, React, Javascript, 
-                and Redux.</h3>
+                and Redux.</p>
 
             {/* <h3>Check out my Projects <a href='/portfolio'>here</a> or learn more about me <a href='/about'>here</a>.</h3> */}
         </div>
--- src/container/ContactPage.js
+++ src/container/ContactPage.js
@@ -7,16 +7,16 @@ export default class ContactPage extends Component {
     render() {
         return (
             <div className='text'>
-                To contact me, please reach out to me on any of the following social media platforms
-                <div>
+                To contact me, please reach out to me on any of the following social media platforms.
+                <div className='social-dash'>
                     <a href='https://twitter.com/WichserBen'>
-                        <img src={twitter} alt='twitter-logo' className='new-tag'/>
+                        <img src={twitter} alt='twitter-logo' className='social-tag'/>
                     </a>
                     <a href='https://www.linkedin.com/in/benjamin-wichser-1b1651179/' >
-                        <img src={linkedIn} alt='linked-in-logo' className='new-tag'/>
+                        <img src={linkedIn} alt='linked-in-logo' className='social-tag'/>
                     </a>
                     <a href='https://github.com/BenWix' >
-                        <img src={github} alt='github-logo'  className='new-tag'/>
+                        <img src={github} alt='github-logo'  className='social-tag'/>
                     </a>
                 </div>
             </div>
