b6681345b62c8cf25c10da8244bc7a7bed15ebde
BenWix <44454993+BenWix@users.noreply.github.com>
2020-09-15 13:58:51 -0400

    Adds reactstrap, adaptive text, and mobile support

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,69 @@
-import React from 'react' 
+import React, {useState} from 'react' 
 import {NavLink} from 'react-router-dom'
+import { Collapse, NavbarToggler, Navbar, Nav, NavItem, NavbarBrand} from 'reactstrap';
 
 
-const Navbar = () => {
-    return (
-        <div className='navbar'>
+const CustomNavbar = (props) => {
+  
+  const isMobile = window.innerWidth <= 500;
+  
+  const [collapsed, setCollapsed] = useState(true)
 
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          className='nav-link link-padding sm-link4'
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink 
-          className='nav-link link-padding sm-link4'
-          style={{ marginRight: '10px' }} 
-          to="/portfolio"
-        >
-          Portfolio
-        </NavLink>
-        <NavLink 
-          style={{ marginRight: '10px' }} 
-          className='nav-link link-padding sm-link4'
-          to="/about"
-        >
-          About Me
-        </NavLink>
-        </div>
+  const toggleNavbar = () => setCollapsed(!collapsed)
+
+  if (isMobile){
+    return(
+
+        <div >
+        <Navbar color="faded" light>
+          <NavbarBrand href="/" className="mr-auto">Ben W</NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink to="/" className='hamburger-link'>Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/portfolio" className='hamburger-link'>Portfolio</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/about" className='hamburger-link'>About Me</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     )
+  } else {
+
+    return (
+      <div className='navbar'>
+
+      <NavLink 
+        style={{ marginRight: '10px' }} 
+        className='nav-link link-padding sm-link4'
+        to="/"
+        >
+        Home
+      </NavLink>
+      <NavLink 
+        className='nav-link link-padding sm-link4'
+        style={{ marginRight: '10px' }} 
+        to="/portfolio"
+        >
+        Portfolio
+      </NavLink>
+      <NavLink 
+        style={{ marginRight: '10px' }} 
+        className='nav-link link-padding sm-link4'
+        to="/about"
+        >
+        About Me
+      </NavLink>
+      </div>
+    )
+  }
 }
 
 
-export default Navbar
+export default CustomNavbar
