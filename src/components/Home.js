5b3b5bb57704675e5bd765796ace99c58a57020e
BenWix <44454993+BenWix@users.noreply.github.com>
2020-09-15 14:07:40 -0400

    Edit words

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,14 +6,13 @@ const Home = () => {
             <h1>Hey, I'm Ben</h1>
             {/* Switch to I statements */}
             <p>I am a software engineer and full-stack web developer with a 
-                background in mechanical engineering and design, I have an 
-                eagerness to tackle difficult coding challenges. I am  great
-                problem solver and critical thinker with excellent project 
-                management abilities. I have experience building full-stack 
-                web applications utilizing Ruby on Rails, React, Javascript, 
-                and Redux.</p>
+                background in mechanical engineering and design. I have an 
+                eagerness to tackle difficult coding challenges and have 
+                experience building full-stack web applications utilizing Ruby 
+                on Rails, React, Javascript, and Redux. Through my work 
+                experience, I have developed into a great problem solver and 
+                critical thinker with excellent project management abilities.</p>
 
-            {/* <h3>Check out my Projects <a href='/portfolio'>here</a> or learn more about me <a href='/about'>here</a>.</h3> */}
         </div>
     )
 }
--- src/container/About.js
+++ src/container/About.js
@@ -6,23 +6,21 @@ export default class About extends Component {
             <div className='about'>
                 <h1>About Ben</h1>
                 <p>I am a mechanical engineer turned web developer. 
-                    My interest in coding was sparked while doing my undergrad at Purdue University. 
-                    After graduating, I spent some time following my passion for environmental causes and 
-                    sustainable living by building tiny houses with a green builder (make sure to 
-                    find me on an episode of HGTV's <em>Tiny House, Big Living</em>). This experience was the 
-                    spark for me to start working in some real estate projects myself!
+                    My interest in coding was sparked during my undergraduate years at Purdue University. 
+                    After graduating, I followed my passion for environmental causes and 
+                    sustainable living by building tiny houses with a green builder (
+                    find me on an episode of HGTV's <em>Tiny House, Big Living</em>). This experience 
+                    increased my enthusiasm for real estate projects.
                 </p> 
                 
-                <p>After this, I gained some more professional experience working as a fire sprinkler design engineer 
-                    and a mechanical design engineer at a machine shop making machines for manufacturing. 
-                    I kept my interest in coding because of the fun challenge coding presented, the culture 
-                    of the tech industry, and the desire for a greener career.
+                <p>When my work building tiny houses came to a close, I gained additional professional experience working as a fire sprinkler design engineer 
+                    and as a mechanical design engineer at a machine shop making machines for manufacturing. 
+                    I wanted to return to coding because of the fun challenges coding presented, tech industry's culture
+                    , and the desire for a greener career.
                 </p>
-                <p>When I am not working on a project
-                    , I am probably having fun outside somewhere. I have been a rock climber for 
-                    my entire adult life and on the weekends I perform and teach acrobatics with my partner,
-                     Meghan. I am an avid birder, 
-                    and I love to go running and mountain biking.
+                <p>When I am not working on a project, I am outside having fun. I have been a rock 
+                    climber for my entire adult life and I am an avid birder. I love to run, mountain bike
+                    , and I perform and teach partner acrobatics with my partner, Meghan.
                 </p>
             </div>
         )
